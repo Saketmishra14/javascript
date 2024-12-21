50cdb5a2fb13fd62d91f735e2f287985d18c9567
saketmishra14 <mishrasaket042@gmail.com>
2024-12-21 16:43:08 +0530

    DOM Manipulation

---
+++ BASICS OF DOM/script.js
@@ -0,0 +1,10 @@
+const x=document.querySelector("h1")
+x.style.backgroundColor="blue"
+let y=document.querySelector("h2")
+y.style.backgroundColor="red"
+y.style.color="yellow"
+const z=document.querySelector("h3")
+z.style.backgroundColor="green"
+z.style.color="yellow"
+
+document.getElementById("#demo").innerText="yash mishra"
---
+++ asyncchrones javascript/script.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+
+setTimeout(() => {
+    console.log("yash mishra");
+    
+    
+}, 1000);
+console.log("last");
