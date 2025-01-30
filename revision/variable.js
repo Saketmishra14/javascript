b51596b58e139e7131efd700a749f560b5d997b0
saket_14 <mishrasaket042@gmail.com>
2025-01-30 07:59:10 +0000

    data types revisit

--- revision/variable.js
+++ revision/variable.js
@@ -1,3 +1,15 @@
 let name="saket"
 const age=87+"8"
 console.log(age)
+var username="yash"
+var username="mishra"
+console.log(username)
+
+//data types 
+let number=200;
+let gretting="hello";
+const value=false;
+ arr=["saket" , 1+1, "yash","magic","3"+5]
+ console.log(arr);
+ const object={name:"saket",age:45,height:55+"cm"}
+ console.log(object);
