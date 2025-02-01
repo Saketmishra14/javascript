f2beaef098dc968e33fa328f5e35616c80415c43
saket_14 <mishrasaket042@gmail.com>
2025-02-01 05:39:58 +0000

    function and scope revisit

---
+++ revision/functionscope.js
@@ -0,0 +1,14 @@
+
+ const greet= function sum(a,b){
+    console.log(a+b);
+    return a+b
+}
+console.log(greet(2,3))
+
+
+//arrow function
+const gree=(name)=>{
+    console.log(`hello, ${name}!`)
+
+}
+gree("yash")
