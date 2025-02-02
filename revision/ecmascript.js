a7ed6bc5a7f51d985bff72cd447def1b218223ad
saket_14 <mishrasaket042@gmail.com>
2025-02-02 10:30:39 +0000

    spread adn rest operators

--- revision/ecmascript.js
+++ revision/ecmascript.js
@@ -11,3 +11,15 @@ let [firstColor, secondColor, thirdColor] = colors;
 console.log(firstColor); // "red"
 console.log(secondColor); // "green"
 console.log(thirdColor); // "blue"
+
+//spread and rest operators
+let numbers1 = [1, 2, 3];
+let numbers2 = [4, 5, 6];
+
+let mergedArray = [...numbers1, ...numbers2]; // Merging both arrays
+console.log(mergedArray); // [1, 2, 3, 4, 5, 6]
+
+//let const var
+
+
+//module (import/export)
