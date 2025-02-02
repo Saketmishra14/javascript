08c469cbdd8d9a5e6608fd770bbba11eecbe5863
saket_14 <mishrasaket042@gmail.com>
2025-02-02 10:15:00 +0000

    ecmascript concept revisit

---
+++ revision/ecmascript.js
@@ -0,0 +1,13 @@
+// template litrals
+let firstname="yash"
+let lastname="mishra"
+ const fullname=`hello everyone, i am ${firstname + lastname}.`
+ console.log(fullname)
+
+ //destructuring (array and object)
+ let colors = ["red", "green", "blue"];
+let [firstColor, secondColor, thirdColor] = colors;
+
+console.log(firstColor); // "red"
+console.log(secondColor); // "green"
+console.log(thirdColor); // "blue"
